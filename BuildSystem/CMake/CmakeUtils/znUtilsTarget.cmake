ef0a6e49f51ac98a2fd564aaf3af22d2549d0275
rrandel-tech <robert.randel46@gmail.com>
2025-09-28 18:09:03 -0400

    Added `apply_compiler_settings` utility and applied compiler settings to targets.

--- BuildSystem/CMake/CmakeUtils/znUtilsTarget.cmake
+++ BuildSystem/CMake/CmakeUtils/znUtilsTarget.cmake
@@ -9,3 +9,48 @@ function(luma_enable_lto target)
 		message(STATUS "  -> IPO enabled for Release builds")
 	endif()
 endfunction()
+
+function(apply_msvc_settings target)
+	target_compile_definitions(${target} PRIVATE
+			$<$<CXX_COMPILER_ID:MSVC>:_CRT_SECURE_NO_WARNINGS>
+			$<$<CXX_COMPILER_ID:MSVC>:_SILENCE_CXX17_CODECVT_HEADER_DEPRECATION_WARNING>
+	)
+
+	target_compile_options(${target} PRIVATE
+			$<$<CXX_COMPILER_ID:MSVC>:/Gy>								# Enable function-level linking
+			$<$<CXX_COMPILER_ID:MSVC>:/EHsc>							# Exception handling model
+			$<$<CXX_COMPILER_ID:MSVC>:/Zc:preprocessor>		# Enable conforming preprocessor
+			$<$<CXX_COMPILER_ID:MSVC>:/Zc:__cplusplus>		# Enable correct __cplusplus macro
+			$<$<CXX_COMPILER_ID:MSVC>:/wd4068>						# Disable "Unknown #pragma mark warning"
+	)
+
+	target_link_options(${target} PRIVATE
+			$<$<CXX_COMPILER_ID:MSVC>:/IGNORE:4099>		# Suppress "no PDB found" warning
+	)
+endfunction()
+
+function(apply_clang_settings target)
+	target_compile_options(${target} PRIVATE
+			$<$<CXX_COMPILER_ID:Clang>:-Wall>
+			$<$<CXX_COMPILER_ID:Clang>:-Wextra>
+			$<$<CXX_COMPILER_ID:Clang>:-Wno-unknown-pragmas>	# Ignore unknown #pragma mark
+			$<$<CXX_COMPILER_ID:Clang>:-ffunction-sections>
+			$<$<CXX_COMPILER_ID:Clang>:-fdata-sections>
+	)
+endfunction()
+
+function(apply_gcc_settings target)
+	target_compile_options(${target} PRIVATE
+			$<$<CXX_COMPILER_ID:GNU>:-Wall>
+			$<$<CXX_COMPILER_ID:GNU>:-Wextra>
+			$<$<CXX_COMPILER_ID:GNU>:-Wno-unknown-pragmas>		# Ignore unknown #pragma mark
+			$<$<CXX_COMPILER_ID:GNU>:-ffunction-sections>
+			$<$<CXX_COMPILER_ID:GNU>:-fdata-sections>
+)
+endfunction()
+
+function(apply_compiler_settings target)
+	apply_msvc_settings(${target})
+	apply_clang_settings(${target})
+	apply_gcc_settings(${target})
+endfunction()
